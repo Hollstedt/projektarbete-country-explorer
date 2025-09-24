adcb35aa8cd853fe5e5fd56819d374e35e356933
Daniel Hollstedt <daniel.hollstedt@gmail.com>
2025-09-24 10:23:38 +0200

    if in quiz-page to render quiz questions from random flags fetched from API and sliced into new array

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -11,6 +11,9 @@ export default function Quiz() {
    const [playersName, setPlayersName] = useState("");
    const [isQuizStarted, setIsQuizStarted] = useState(false);
    const [randomizedQuizCountries, setRandomizedQuizCountries] = useState([]);
+   const [score, setScore] = useState(0);
+   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+   const [userAnswer, setUserAnswer] = useState("");
 
 
    const handleRegionChange = (event) => {
@@ -23,19 +26,32 @@ export default function Quiz() {
          const randomCountries = countries.sort(() => 0.5 - Math.random());
          const selectedRandomCountries = randomCountries.slice(0, 15);
 
+         console.log("Länder hämtade från API:", countries.length);
+         console.log("Array med random hämtade länder:", selectedRandomCountries, "och det är totalt", selectedRandomCountries.length, "st.")
+
          setRandomizedQuizCountries(selectedRandomCountries)
          setIsQuizStarted(true)
-         
       } else {
          alert("Ange namn och välj världsdel!")
       }
    }
 
    if (isQuizStarted) {
+      const currentCountryForQuiz = randomizedQuizCountries[currentQuestionIndex];
+
       return (
          <div>
             <h1>Quiz startat för {playersName}</h1>
             <h2>Vald världsdel: {selectedRegion}</h2>
+            <p>Fråga {currentQuestionIndex + 1} av 15</p>
+            <p>Poäng: {score}</p>
+
+            <div>
+               <img src={currentCountryForQuiz.flags.svg} alt="Flagga av landet xx" width="200" />
+               <p>Vilket land är det här?</p>
+               <input type="text" value={userAnswer} onChange={(event) => setUserAnswer(event.target.value)} placeholder="Ditt svar här..." />
+               <button onClick={() => console.log("Användaren svarade:", userAnswer)}>Svara</button>
+            </div>
          </div>
       )
    }
